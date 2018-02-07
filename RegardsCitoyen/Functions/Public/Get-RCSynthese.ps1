fc4d19b8852cdb521fd1fd6ddd1c3cc39346ff70
Van Gulick Stéphane <Stephane.VanGulick@swisscom.com>
2018-02-07 01:10:53 +0100

    Added Get-RcSynthese with test

--- RegardsCitoyen/Functions/Public/Get-RCSynthese.ps1
+++ RegardsCitoyen/Functions/Public/Get-RCSynthese.ps1
@@ -3,15 +3,29 @@ Function Get-RCSynthese {
     <#
     .SYNOPSIS
     Get-RCSynthese 
-    Attention, le results retournées concernent uniquement les parlementaires qui ont repondu.
+    Attention, le results retournées concernent uniquement les parlementaires qui ont effectué une activité.
+
     .DESCRIPTION
     Long description
     
     .PARAMETER Date
-    Parameter description
+    Le format a resecter est le suivant:
+        AAAAMM
+
+        AAAA Corrspond a l'année sur quatre chiffres Example -> 2017
+        MM correspond au mois voulu Exemple -> 04 (avril)
     
+        Voir exemples plus bas.
+
     .EXAMPLE
-    An example
+    Get-RcSynthese
+
+    REtourne les données de synthese de tous les mois de cette législature.
+
+    .EXAMPLE
+    Get-RcSynthese -Date 201704
+
+    REtourne les données de synthese du mois d'avril de 2017
     
     .NOTES
     General notes
@@ -26,8 +40,7 @@ Function Get-RCSynthese {
     )
 
     
-    #https://www.nosdeputes.fr/synthese/$mois/Json
-    #synthese = "https://www.nosdeputes.fr/synthese/"
+    #[Synthese] L'API retourne des fois un groupe vide. (Erreur?) Ca fait planter le constructeur Synthese
     $url = ($RC_data.Urls.Synthese).Replace("data/json",$Date) + "/json"
 
     
@@ -40,7 +53,14 @@ Function Get-RCSynthese {
     $Data = Invoke-RestMethod @Parameters
 
     Foreach ($syn in $Data.Deputes.Depute){
-        [Synthese]::New($syn.id,$syn.nom,$syn.Groupe,$syn.Amendements_Adoptes,$syn.Amendements_Proposes,$syn.Amendements_Signes,$syn.Commision_Interventions,$syn.Commision_Presences,$syn.hemicycle_Interventions,$syn.hemicycle_Interventions_Courtes,$syn.Proposition_Ecrites,$syn.Propositions_Signees,$syn.Questions_Ecrites,$syn.Questions_Orales,$syn.Rapports,$syn.Semaines_Presence)
+        try{
+
+            [Synthese]::New($syn.id,$syn.nom,$syn.Groupe,$syn.Amendements_Adoptes,$syn.Amendements_Proposes,$syn.Amendements_Signes,$syn.Commision_Interventions,$syn.Commision_Presences,$syn.hemicycle_Interventions,$syn.hemicycle_Interventions_Courtes,$syn.Proposition_Ecrites,$syn.Propositions_Signees,$syn.Questions_Ecrites,$syn.Questions_Orales,$syn.Rapports,$syn.Semaines_Presence)
+        }Catch{
+            $_
+        }
     }
 
 }
+
+Get-RCSynthese
---
+++ Tests/Get-RCSynthese.Tests.ps1
@@ -0,0 +1,58 @@
+$TestsPath = Split-Path $MyInvocation.MyCommand.Path
+
+#$FunctionsPath = join-Path -Path (get-item $TestsPath).Parent -ChildPath "Functions"
+
+$RootFolder = (get-item $TestsPath).Parent
+
+Push-Location -Path $RootFolder.FullName
+
+set-location -Path $RootFolder.FullName
+
+Write-Verbose "Importing module"
+
+import-module .\RegardsCitoyen -Force
+
+Context "Testing Regards Citoyen"{
+    Describe "Get-RcSynthese" {
+
+        it "Should not throw an error" {
+            {Get-RCSynthese} | should not throw
+        }
+
+        it "Should return list of Syntheses (Empty Param)" {
+            $obj = Get-RCSynthese
+
+            $obj | should not benullorempty
+
+            $obj.count -gt 2 | should be $true
+        }
+
+        it "Should return object of type 'Synthese' (Empty Param)" {
+            $obj = Get-RCSynthese
+
+            $r = Get-Random -Minimum 1 -Maximum $obj.count
+            $obj[$r].GetType().Name | should be "Synthese"
+
+            
+        }
+
+        it "Should return only one object (Param Date '201711' )" {
+            $now = Get-Date
+            $now.AddMonths(-6)
+            $obj = Get-RCSynthese -Date "201711"
+            $obj.count -ge 1 | should be $true
+
+        }
+
+        it "Should throw with wrong date format (Param Date '2011' )" {
+            {Get-RCSynthese -Date "2011"} | should throw
+            
+
+        }
+
+    
+    }
+    
+}
+
+Pop-Location
